14d7d26cf3e5e564756144941a08998f7149f59c
Martin Filo <martinxfilo@gmail.com>
2018-08-17 23:57:10 +0200

    landing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PortComponent } from './components/port/port.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PortComponent
   ],
   imports: [
     BrowserModule
